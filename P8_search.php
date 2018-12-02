f53657e33ce34696ea5658a2535619d2023708f9
葉如芳 <B10423016@yuntech.org.tw>
2018-12-03 01:57:33 +0800

    Add files via upload

---
+++ P8_search.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>P8_search.php</title>
+</head>
+<body>
+    <center><h1>基本資料管理系統-查詢</h1></center>
+    <hr size="1px" align="center" width="100%">
+    <center><form method="post" action="P8_success.php">
+        <p>編號：<input type="text" name="No"></p>
+        
+        <input type="submit" name="Query" value="查詢">
+        <input type="reset" value="清除">
+    </form></center>
+    <hr size="1px" align="center" width="100%">
+</body>
+</html>
